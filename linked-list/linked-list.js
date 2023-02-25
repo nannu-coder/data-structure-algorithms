f5decfb302a989ce6368866a05e4f5fd16c10e01
Nazmul Hasan Nannu <nazmulnannu43@gmail.com>
2023-02-25 12:16:19 +0600

    error fixed

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -1,32 +1,38 @@
-// Linked List Implementation in javascript
-
-// Creating Node
+//Create Node
 class Node {
-  constructor() {
-    (this.value = 0), (this.next = null);
+  constructor(value) {
+    (this.value = value), (this.next = null);
   }
 }
 
-let head = null;
-let one = null;
-let two = null;
-let three = null;
-
-//Allocate three node in the heap
-one = new Node();
-two = new Node();
-three = new Node();
-
-//Assign value in values
-one.value = 1;
-two.value = 2;
-three.value = 3;
-
-//print linked list value
-head = one;
-
-while (head != null) {
-  // document.write(head.value)
-  console.log(head.value);
-  head = head.next;
+//Create List
+class Linkedlist {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
 }
+
+const list = new Linkedlist(4);
+list.append(5);
+list.append(7);
+list.prepend(1);
+console.log(list);
